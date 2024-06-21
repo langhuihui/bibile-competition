46c0583922d73569c4c45ab2499ca47012d32cf2
dexter <178529795@qq.com>
2024-06-21 22:57:45 +0800

    修改外观

--- src/components/Big.vue
+++ src/components/Big.vue
@@ -1,18 +1,22 @@
 <template>
     <n-config-provider :theme-overrides="themeOverrides">
       <n-layout>
-        <n-layout-content>
+        <n-layout-header>
+          <n-space><n-input-number v-for="n of group" :default-value="0"></n-input-number></n-space>
+        </n-layout-header>
+        <n-layout-content style="width:80vw;height:70vh">
           <n-card>
             <n-space vertical>
               <div class="question">{{ currentQuestion.question }}</div>
               <n-space>
                 <n-radio-group v-model:value="selectedOption">
-                  <n-radio-button class="option" :label="option" v-for="(option,optionIndex ) in currentQuestion.answers" :key="option" :value="optionIndex"></n-radio-button>
+                  <n-radio-button class="option" size="large" :label="option" v-for="(option,optionIndex ) in currentQuestion.answers" :key="option" :value="optionIndex"></n-radio-button>
                 </n-radio-group>
               </n-space>
               <n-space justify="end">
-                <n-button @click="submitAnswer">提交</n-button>
+                <n-button @click="submitAnswer" size="large">提交</n-button>
               </n-space>
+             
             </n-space>
             <n-alert v-if="showResult" :type="isCorrect ? 'success' : 'error'" title="结果">
               {{ isCorrect ? '回答正确！' : '回答错误！正确答案：'+currentQuestion.answers[currentQuestion.correctAnswer] }}
@@ -20,18 +24,21 @@
             <n-button v-if="showNextButton" @click="nextQuestion">下一题</n-button>
           </n-card>
         </n-layout-content>
+        <n-layout-footer>
+          <n-slider v-model:value="currentQuestionIndex" />
+        </n-layout-footer>
       </n-layout>
     </n-config-provider>
   </template>
   
   <script setup>
-  import { ref } from 'vue';
-  import { NConfigProvider, NLayout, NLayoutContent, NLayoutHeader, NLayoutSider, NLayoutFooter, NMenu, NButton, NRadioGroup, NRadio, NCard, NAlert, NSpace, NRadioButton } from 'naive-ui';
+  import { computed, ref } from 'vue';
+  import {NSlider,NInputNumber, NConfigProvider, NLayout, NLayoutContent, NLayoutHeader, NLayoutSider, NLayoutFooter, NMenu, NButton, NRadioGroup, NRadio, NCard, NAlert, NSpace, NRadioButton } from 'naive-ui';
   import questions from './question.json'
  
-  
+  const group = ref(7)
   const currentQuestionIndex = ref(0);
-  const currentQuestion = ref(questions[currentQuestionIndex.value]);
+  const currentQuestion = computed(()=>questions[currentQuestionIndex.value])
   const selectedOption = ref(null);
   const showResult = ref(false);
   const isCorrect = ref(false);
@@ -45,7 +52,6 @@
   
   const nextQuestion = () => {
     currentQuestionIndex.value++;
-    currentQuestion.value = questions[currentQuestionIndex.value];
     selectedOption.value = null;
     showResult.value = false;
     showNextButton.value = false;
@@ -63,13 +69,13 @@
   </script>
   <style>
 .question {
-  font-size: 8rem;
+  font-size: 2rem;
 }
 .option {
-  font-size: 4rem;
-  min-height: 100px;
-  min-width: 300px;
-  margin: 10px;
-  line-height: 4rem;
+  /* font-size: 2rem; */
+  /* min-height: 100px; */
+  /* min-width: 300px; */
+  /* margin: 10px; */
+  /* line-height: 4rem; */
 }
 </style>
